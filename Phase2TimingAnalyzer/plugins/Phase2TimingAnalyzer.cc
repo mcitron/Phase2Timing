4579cc4bb8643ad7a008b527b42b0beb0860a386
matthew <matthewdc2@gmail.com>
2021-12-06 09:19:15 -0800

    made gen stuff vectors

--- Phase2TimingAnalyzer/plugins/Phase2TimingAnalyzer.cc
+++ Phase2TimingAnalyzer/plugins/Phase2TimingAnalyzer.cc
@@ -18,6 +18,7 @@
 
 // system include files
 #include <memory>
+#include "TLorentzVector.h"
 
 // user include files
 #include "FWCore/Framework/interface/Frameworkfwd.h"
@@ -50,55 +51,20 @@
 // class declaration
 //
 struct tree_struc_{
-  float                         q1_eta;
-  float                         q2_eta;
-  float                         q3_eta;
-  float                         q4_eta;
-  float                         q1_phi;
-  float                         q2_phi;
-  float                         q3_phi;
-  float                         q4_phi;
-  float                         q1_pt;
-  float                         q2_pt;
-  float                         q3_pt;
-  float                         q4_pt;
-  float                         q1_vx;
-  float                         q2_vx;
-  float                         q3_vx;
-  float                         q4_vx;
-  float                         q1_vy;
-  float                         q2_vy;
-  float                         q3_vy;
-  float                         q4_vy;
-  float                         q1_vz;
-  float                         q2_vz;
-  float                         q3_vz;
-  float                         q4_vz;
-  float			q1_ebeta;
-  float			q1_ebphi;
-  float			q1_ebdelay;
-  float			q2_ebeta;
-  float			q2_ebphi;
-  float			q2_ebdelay;
-  float			q3_ebeta;
-  float			q3_ebphi;
-  float			q3_ebdelay;
-  float			q4_ebeta;
-  float			q4_ebphi;
-  float			q4_ebdelay;
-  float			q1_hgeta;
-  float			q1_hgphi;
-  float			q1_hgdelay;
-  float			q2_hgeta;
-  float			q2_hgphi;
-  float			q2_hgdelay;
-  float			q3_hgeta;
-  float			q3_hgphi;
-  float			q3_hgdelay;
-  float			q4_hgeta;
-  float			q4_hgphi;
-  float			q4_hgdelay;
+    std::vector<float> q_eta;
+    std::vector<float> q_phi;
+    std::vector<float> q_pt;
+    std::vector<float> q_vx;
+    std::vector<float> q_vy;
+    std::vector<float> q_vz;
+    std::vector<float> q_ebeta;
+    std::vector<float> q_ebphi;
+    std::vector<float> q_ebdelay;
+    std::vector<float> q_hgeta;
+    std::vector<float> q_hgphi;
+    std::vector<float> q_hgdelay;
   int                           nrecojets;
+  int                           ngen;
   std::vector<float>            recojet_pt;
   std::vector<float>            recojet_eta;
   std::vector<float>            recojet_phi;
@@ -112,6 +78,12 @@ struct tree_struc_{
   std::vector<float>            recojet_HGCALtime;
   std::vector<float>            recojet_HGCALenergy;
   std::vector<float>            recojet_HGCALnTracksters;
+  std::vector<float>  recojet_closestGenIndex;
+  std::vector<float>  recojet_closestGenR;
+  std::vector<float>  recojet_closestEbGenIndex;
+  std::vector<float>  recojet_closestEbGenR;
+  std::vector<float>  recojet_closestHgGenIndex;
+  std::vector<float>  recojet_closestHgGenR;
 };
 
 // If the analyzer does not use TFileService, please remove
@@ -227,56 +199,21 @@ void Phase2TimingAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSet
   //  auto const& ecalRecHitsEB = iEvent.get(ecalRecHitsEBToken_);
 
   //variable declaration
-  float q1_pt  = -10000;
-  float q2_pt  = -10000;
-  float q3_pt  = -10000;
-  float q4_pt  = -10000;
-  float q1_phi  = -10000;
-  float q2_phi  = -10000;
-  float q3_phi  = -10000;
-  float q4_phi  = -10000;
-  float q1_eta  = -10000;
-  float q2_eta  = -10000;
-  float q3_eta  = -10000;
-  float q4_eta  = -10000;
-  float q1_vx   = -10000;
-  float q1_vy   = -10000;
-  float q1_vz   = -10000;
-  float q2_vx   = -10000;
-  float q2_vy   = -10000;
-  float q2_vz   = -10000;
-  float q3_vx   = -10000;
-  float q3_vy   = -10000;
-  float q3_vz   = -10000;
-  float q4_vx   = -10000;
-  float q4_vy   = -10000;
-  float q4_vz   = -10000;
-  float q1_ebeta = -10000;
-  float q1_ebphi = -10000;
-  float q1_ebdelay = -10000;
-  float q2_ebeta = -10000;
-  float q2_ebphi = -10000;
-  float q2_ebdelay = -10000;
-  float q3_ebeta = -10000;
-  float q3_ebphi = -10000;
-  float q3_ebdelay = -10000;
-  float q4_ebeta = -10000;
-  float q4_ebphi = -10000;
-  float q4_ebdelay = -10000;
-  float q1_hgeta = -10000;
-  float q1_hgphi = -10000;
-  float q1_hgdelay = -10000;
-  float q2_hgeta = -10000;
-  float q2_hgphi = -10000;
-  float q2_hgdelay = -10000;
-  float q3_hgeta = -10000;
-  float q3_hgphi = -10000;
-  float q3_hgdelay = -10000;
-  float q4_hgeta = -10000;
-  float q4_hgphi = -10000;
-  float q4_hgdelay = -10000;
   int interestingquark = 0;
   int nrecojets = 0;
+  int ngen = 0;
+  std::vector<float> q_eta;
+  std::vector<float> q_phi;
+  std::vector<float> q_pt;
+  std::vector<float> q_vx;
+  std::vector<float> q_vy;
+  std::vector<float> q_vz;
+  std::vector<float> q_ebeta;
+  std::vector<float> q_ebphi;
+  std::vector<float> q_ebdelay;
+  std::vector<float> q_hgeta;
+  std::vector<float> q_hgphi;
+  std::vector<float> q_hgdelay;
   std::vector<float>    recojet_pt;
   std::vector<float>    recojet_eta;
   std::vector<float>    recojet_phi;
@@ -290,6 +227,12 @@ void Phase2TimingAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSet
   std::vector<float>    recojet_HGCALtime;
   std::vector<float>    recojet_HGCALenergy;
   std::vector<float>    recojet_HGCALnTracksters;
+  std::vector<float>  recojet_closestGenIndex;
+  std::vector<float>  recojet_closestGenR;
+  std::vector<float>  recojet_closestEbGenIndex;
+  std::vector<float>  recojet_closestEbGenR;
+  std::vector<float>  recojet_closestHgGenIndex;
+  std::vector<float>  recojet_closestHgGenR;
 
   
   bool debug=0;
@@ -307,20 +250,19 @@ void Phase2TimingAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSet
     reco::GenParticle * genParticleMother = (reco::GenParticle *) genpar_iter.mother();
     std::vector<double> ecalIntersection = _jetTimingTools.surfaceIntersection(genpar_iter,*genParticleMother,130);
     std::vector<double> hgcalIntersection = _jetTimingTools.endCapIntersection(genpar_iter,*genParticleMother,300,520);
-
-    if (interestingquark==1){ q1_pt = genpar_iter.pt(); q1_eta = genpar_iter.eta(); q1_phi = genpar_iter.phi(); q1_vx = vx; q1_vy = vy; q1_vz = vz; 
-	q1_ebeta=ecalIntersection[0];q1_ebphi=ecalIntersection[1];q1_ebdelay=ecalIntersection[3];
-	q1_hgeta=hgcalIntersection[0];q1_hgphi=hgcalIntersection[1];q1_hgdelay=hgcalIntersection[3];}
-    if (interestingquark==2){ q2_pt = genpar_iter.pt(); q2_eta = genpar_iter.eta(); q2_phi = genpar_iter.phi(); q2_vx = vx; q2_vy = vy; q2_vz = vz;
-	q2_ebeta=ecalIntersection[0];q2_ebphi=ecalIntersection[1];q2_ebdelay=ecalIntersection[3];
-	q2_hgeta=hgcalIntersection[0];q2_hgphi=hgcalIntersection[1];q2_hgdelay=hgcalIntersection[3];}
-    if (interestingquark==3){ q3_pt = genpar_iter.pt(); q3_eta = genpar_iter.eta(); q3_phi = genpar_iter.phi(); q3_vx = vx; q3_vy = vy; q3_vz = vz;
-	q3_ebeta=ecalIntersection[0];q3_ebphi=ecalIntersection[1];q3_ebdelay=ecalIntersection[3];
-	q3_hgeta=hgcalIntersection[0];q3_hgphi=hgcalIntersection[1];q3_hgdelay=hgcalIntersection[3];}
-    if (interestingquark==4){ q4_pt = genpar_iter.pt(); q4_eta = genpar_iter.eta(); q4_phi = genpar_iter.phi(); q4_vx = vx; q4_vy = vy; q4_vz = vz;
-	q4_ebeta=ecalIntersection[0];q4_ebphi=ecalIntersection[1];q4_ebdelay=ecalIntersection[3];
-	q4_hgeta=hgcalIntersection[0];q4_hgphi=hgcalIntersection[1];q4_hgdelay=hgcalIntersection[3];}
-
+    ngen++;
+    q_pt.push_back(genpar_iter.pt());
+    q_eta.push_back(genpar_iter.eta());
+    q_phi.push_back(genpar_iter.phi());
+    q_vx.push_back(vx);
+    q_vy.push_back(vy);
+    q_vz.push_back(vz);
+    q_ebphi.push_back(ecalIntersection[0]);
+    q_ebeta.push_back(ecalIntersection[1]);
+    q_ebdelay.push_back(ecalIntersection[3]);
+    q_hgphi.push_back(hgcalIntersection[0]);
+    q_hgeta.push_back(hgcalIntersection[1]);
+    q_hgdelay.push_back(hgcalIntersection[3]);
   }
 
   auto const& ecalRecHitsEB = iEvent.get(ecalRecHitsEBToken_);
@@ -338,6 +280,44 @@ void Phase2TimingAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSet
     recojet_eta.push_back(recojet_iter.eta());
     recojet_phi.push_back(recojet_iter.phi());
     recojet_e.push_back(recojet_iter.energy());
+    int closestGenIndex = -1;
+    float closestGenR = 999;
+    int closestEbGenIndex = -1;
+    float closestEbGenR = 999;
+    int closestHgGenIndex = -1;
+    float closestHgGenR = 999;
+    for (int ig = 0; ig < ngen; ig++){
+	TLorentzVector jetVec;
+	jetVec.SetPtEtaPhiM(recojet_iter.pt(),recojet_iter.eta(),recojet_iter.phi(),0);
+	TLorentzVector pos;
+	pos.SetPtEtaPhiM(1,q_eta[ig],q_phi[ig],0);
+	TLorentzVector ebPos;
+	ebPos.SetPtEtaPhiM(1,q_ebeta[ig],q_ebphi[ig],0);
+	TLorentzVector hgPos;
+	hgPos.SetPtEtaPhiM(1,q_hgeta[ig],q_hgphi[ig],0);
+	
+	float ebDeltaR = jetVec.DeltaR(ebPos);
+	if (ebDeltaR < 0.16 and closestEbGenR > ebDeltaR){
+	    closestEbGenR = ebDeltaR;
+	    closestEbGenIndex = ig;
+	}
+	float hgDeltaR = jetVec.DeltaR(hgPos);
+	if (hgDeltaR < 0.16 and closestHgGenR > hgDeltaR){
+	    closestHgGenR = hgDeltaR;
+	    closestHgGenIndex = ig;
+	}
+	float genDeltaR = jetVec.DeltaR(pos);
+	if (genDeltaR < 0.16 and closestGenR > genDeltaR){
+	    closestGenR = genDeltaR;
+	    closestGenIndex = ig;
+	}
+    }
+    recojet_closestGenIndex.push_back(closestGenIndex);
+    recojet_closestGenR.push_back(closestGenR);
+    recojet_closestEbGenIndex.push_back(closestEbGenIndex);
+    recojet_closestEbGenR.push_back(closestEbGenR);
+    recojet_closestHgGenIndex.push_back(closestHgGenIndex);
+    recojet_closestHgGenR.push_back(closestHgGenR);
     
 
     if(debug)std::cout<<" [DEBUG MODE] ------------- COMPUTE JET TIME FROM ECAL --------------------------------------"<<std::endl; 
@@ -409,57 +389,29 @@ void Phase2TimingAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSet
   // --- setup tree values                                                                                                                                   
   initTreeStructure();
   clearVectors();
-  tree_.q1_eta = q1_eta;
-  tree_.q2_eta = q2_eta;
-  tree_.q3_eta = q3_eta;
-  tree_.q4_eta = q4_eta;
-  tree_.q1_phi = q1_phi;
-  tree_.q2_phi = q2_phi;
-  tree_.q3_phi = q3_phi;
-  tree_.q4_phi = q4_phi;
-  tree_.q1_ebeta = q1_ebeta;
-  tree_.q2_ebeta = q2_ebeta;
-  tree_.q3_ebeta = q3_ebeta;
-  tree_.q4_ebeta = q4_ebeta;
-  tree_.q1_ebphi = q1_ebphi;
-  tree_.q2_ebphi = q2_ebphi;
-  tree_.q3_ebphi = q3_ebphi;
-  tree_.q4_ebphi = q4_ebphi;
-  tree_.q1_ebdelay = q1_ebdelay;
-  tree_.q2_ebdelay = q2_ebdelay;
-  tree_.q3_ebdelay = q3_ebdelay;
-  tree_.q4_ebdelay = q4_ebdelay;
-  tree_.q1_hgeta = q1_hgeta;
-  tree_.q2_hgeta = q2_hgeta;
-  tree_.q3_hgeta = q3_hgeta;
-  tree_.q4_hgeta = q4_hgeta;
-  tree_.q1_hgphi = q1_hgphi;
-  tree_.q2_hgphi = q2_hgphi;
-  tree_.q3_hgphi = q3_hgphi;
-  tree_.q4_hgphi = q4_hgphi;
-  tree_.q1_hgdelay = q1_hgdelay;
-  tree_.q2_hgdelay = q2_hgdelay;
-  tree_.q3_hgdelay = q3_hgdelay;
-  tree_.q4_hgdelay = q4_hgdelay;
-  tree_.q1_pt = q1_pt;
-  tree_.q2_pt = q2_pt;
-  tree_.q3_pt = q3_pt;
-  tree_.q4_pt = q4_pt;
-  tree_.q1_vx = q1_vx;
-  tree_.q2_vx = q2_vx;
-  tree_.q3_vx = q3_vx;
-  tree_.q4_vx = q4_vx;
-  tree_.q1_vy = q1_vy;
-  tree_.q2_vy = q2_vy;
-  tree_.q3_vy = q3_vy;
-  tree_.q4_vy = q4_vy;
-  tree_.q1_vz = q1_vz;
-  tree_.q2_vz = q2_vz;
-  tree_.q3_vz = q3_vz;
-  tree_.q4_vz = q4_vz;
-
+  tree_.ngen        = ngen;
+  for (int ig = 0; ig < ngen; ig++){
+    tree_.q_pt.push_back(q_pt[ig]);
+    tree_.q_eta.push_back(q_eta[ig]);
+    tree_.q_phi.push_back(q_phi[ig]); 
+    tree_.q_vx.push_back(q_vx[ig]);
+    tree_.q_vy.push_back(q_vy[ig]);
+    tree_.q_vz.push_back(q_vz[ig]); 
+    tree_.q_ebphi.push_back(q_ebphi[ig]); 
+    tree_.q_ebeta.push_back(q_ebeta[ig]); 
+    tree_.q_ebdelay.push_back(q_ebdelay[ig]); 
+    tree_.q_hgphi.push_back(q_hgphi[ig]); 
+    tree_.q_hgeta.push_back(q_hgeta[ig]); 
+    tree_.q_hgdelay.push_back(q_hgdelay[ig]); 
+  }
   tree_.nrecojets        = nrecojets;
   for (int ij = 0; ij < nrecojets; ij++){
+    tree_.recojet_closestGenIndex.push_back(recojet_closestGenIndex[ij]);
+    tree_.recojet_closestGenR.push_back(recojet_closestGenR[ij]);
+    tree_.recojet_closestEbGenIndex.push_back(recojet_closestEbGenIndex[ij]);
+    tree_.recojet_closestEbGenR.push_back(recojet_closestEbGenR[ij]);
+    tree_.recojet_closestHgGenIndex.push_back(recojet_closestHgGenIndex[ij]);
+    tree_.recojet_closestHgGenR.push_back(recojet_closestHgGenR[ij]);
     tree_.recojet_pt.push_back(recojet_pt[ij]);
     tree_.recojet_eta.push_back(recojet_eta[ij]);
     tree_.recojet_phi.push_back(recojet_phi[ij]); 
@@ -488,54 +440,20 @@ void Phase2TimingAnalyzer::beginJob() {
   if (verbose_) std::cout << "Starting job" << std::endl;
   // --- set up output tree                                                                                                                                  
   tree = fs->make<TTree>("tree","tree");
-  tree->Branch("q1_eta", &tree_.q1_eta, "q1_eta/F");
-  tree->Branch("q2_eta", &tree_.q2_eta, "q2_eta/F");
-  tree->Branch("q3_eta", &tree_.q3_eta, "q3_eta/F");
-  tree->Branch("q4_eta", &tree_.q4_eta, "q4_eta/F");
-  tree->Branch("q1_phi", &tree_.q1_phi, "q1_phi/F");
-  tree->Branch("q2_phi", &tree_.q2_phi, "q2_phi/F");
-  tree->Branch("q3_phi", &tree_.q3_phi, "q3_phi/F");
-  tree->Branch("q4_phi", &tree_.q4_phi, "q4_phi/F");
-  tree->Branch("q1_pt", &tree_.q1_pt, "q1_pt/F");
-  tree->Branch("q2_pt", &tree_.q2_pt, "q2_pt/F");
-  tree->Branch("q3_pt", &tree_.q3_pt, "q3_pt/F");
-  tree->Branch("q4_pt", &tree_.q4_pt, "q4_pt/F");
-  tree->Branch("q1_vx", &tree_.q1_vx, "q1_vx/F");
-  tree->Branch("q1_ebeta", &tree_.q1_ebeta, "q1_ebeta/F");
-  tree->Branch("q2_ebeta", &tree_.q2_ebeta, "q2_ebeta/F");
-  tree->Branch("q3_ebeta", &tree_.q3_ebeta, "q3_ebeta/F");
-  tree->Branch("q4_ebeta", &tree_.q4_ebeta, "q4_ebeta/F");
-  tree->Branch("q1_ebphi", &tree_.q1_ebphi, "q1_ebphi/F");
-  tree->Branch("q2_ebphi", &tree_.q2_ebphi, "q2_ebphi/F");
-  tree->Branch("q3_ebphi", &tree_.q3_ebphi, "q3_ebphi/F");
-  tree->Branch("q4_ebphi", &tree_.q4_ebphi, "q4_ebphi/F");
-  tree->Branch("q1_ebdelay", &tree_.q1_ebdelay, "q1_ebdelay/F");
-  tree->Branch("q2_ebdelay", &tree_.q2_ebdelay, "q2_ebdelay/F");
-  tree->Branch("q3_ebdelay", &tree_.q3_ebdelay, "q3_ebdelay/F");
-  tree->Branch("q4_ebdelay", &tree_.q4_ebdelay, "q4_ebdelay/F");
-  tree->Branch("q1_hgeta", &tree_.q1_hgeta, "q1_hgeta/F");
-  tree->Branch("q2_hgeta", &tree_.q2_hgeta, "q2_hgeta/F");
-  tree->Branch("q3_hgeta", &tree_.q3_hgeta, "q3_hgeta/F");
-  tree->Branch("q4_hgeta", &tree_.q4_hgeta, "q4_hgeta/F");
-  tree->Branch("q1_hgphi", &tree_.q1_hgphi, "q1_hgphi/F");
-  tree->Branch("q2_hgphi", &tree_.q2_hgphi, "q2_hgphi/F");
-  tree->Branch("q3_hgphi", &tree_.q3_hgphi, "q3_hgphi/F");
-  tree->Branch("q4_hgphi", &tree_.q4_hgphi, "q4_hgphi/F");
-  tree->Branch("q1_hgdelay", &tree_.q1_hgdelay, "q1_hgdelay/F");
-  tree->Branch("q2_hgdelay", &tree_.q2_hgdelay, "q2_hgdelay/F");
-  tree->Branch("q3_hgdelay", &tree_.q3_hgdelay, "q3_hgdelay/F");
-  tree->Branch("q4_hgdelay", &tree_.q4_hgdelay, "q4_hgdelay/F");
-  tree->Branch("q2_vx", &tree_.q2_vx, "q2_vx/F");
-  tree->Branch("q3_vx", &tree_.q3_vx, "q3_vx/F");
-  tree->Branch("q4_vx", &tree_.q4_vx, "q4_vx/F");
-  tree->Branch("q1_vy", &tree_.q1_vy, "q1_vy/F");
-  tree->Branch("q2_vy", &tree_.q2_vy, "q2_vy/F");
-  tree->Branch("q3_vy", &tree_.q3_vy, "q3_vy/F");
-  tree->Branch("q4_vy", &tree_.q4_vy, "q4_vy/F");
-  tree->Branch("q1_vz", &tree_.q1_vz, "q1_vz/F");
-  tree->Branch("q2_vz", &tree_.q2_vz, "q2_vz/F");
-  tree->Branch("q3_vz", &tree_.q3_vz, "q3_vz/F");
-  tree->Branch("q4_vz", &tree_.q4_vz, "q4_vz/F");
+  tree->Branch("ngen",              &tree_.ngen,                "ngen/I");
+  tree->Branch("q_eta", &tree_.q_eta);
+  tree->Branch("q_phi", &tree_.q_phi);
+  tree->Branch("q_pt", &tree_.q_pt);
+  tree->Branch("q_vx", &tree_.q_vx);
+  tree->Branch("q_ebeta", &tree_.q_ebeta);
+  tree->Branch("q_ebphi", &tree_.q_ebphi);
+  tree->Branch("q_ebdelay", &tree_.q_ebdelay);
+  tree->Branch("q_hgeta", &tree_.q_hgeta);
+  tree->Branch("q_hgphi", &tree_.q_hgphi);
+  tree->Branch("q_hgdelay", &tree_.q_hgdelay);
+  tree->Branch("q_vx", &tree_.q_vx);
+  tree->Branch("q_vy", &tree_.q_vy);
+  tree->Branch("q_vz", &tree_.q_vz);
 
   tree->Branch("nrecojets",              &tree_.nrecojets,                "nrecojets/I");
   tree->Branch("recoJet_pt",             &tree_.recojet_pt);
@@ -551,61 +469,18 @@ void Phase2TimingAnalyzer::beginJob() {
   tree->Branch("recoJet_HGCALtime",             &tree_.recojet_HGCALtime);
   tree->Branch("recoJet_HGCALenergy",             &tree_.recojet_HGCALenergy);
   tree->Branch("recoJet_HGCALnTracksters",             &tree_.recojet_HGCALnTracksters);
+  tree->Branch("recoJet_closestGenIndex",&tree_.recojet_closestGenIndex);
+  tree->Branch("recoJet_closestGenR",&tree_.recojet_closestGenR);
+  tree->Branch("recoJet_closestEbGenIndex",&tree_.recojet_closestEbGenIndex);
+  tree->Branch("recoJet_closestEbGenR",&tree_.recojet_closestEbGenR);
+  tree->Branch("recoJet_closestHgGenIndex",&tree_.recojet_closestHgGenIndex);
+  tree->Branch("recoJet_closestHgGenR",&tree_.recojet_closestHgGenR);
 }
 
 
 // ------------ initialize trees ------------                                                                                                                 
 void Phase2TimingAnalyzer::initTreeStructure()
 {
-  tree_.q1_pt = -999;
-  tree_.q2_pt = -999;
-  tree_.q3_pt = -999;
-  tree_.q4_pt = -999;
-  tree_.q1_eta = -999;
-  tree_.q2_eta = -999;
-  tree_.q3_eta = -999;
-  tree_.q4_eta = -999;
-  tree_.q1_phi = -999;
-  tree_.q2_phi = -999;
-  tree_.q3_phi = -999;
-  tree_.q4_phi = -999;
-  tree_.q1_vx = -999;
-  tree_.q2_vx = -999;
-  tree_.q3_vx = -999;
-  tree_.q4_vx = -999;
-  tree_.q1_vy = -999;
-  tree_.q2_vy = -999;
-  tree_.q3_vy = -999;
-  tree_.q4_vy = -999;
-  tree_.q1_vz = -999;
-  tree_.q2_vz = -999;
-  tree_.q3_vz = -999;
-  tree_.q4_vz = -999;
-  tree_.q1_ebeta = -999;
-  tree_.q1_ebphi = -999;
-  tree_.q1_ebdelay = -999;
-  tree_.q2_ebeta = -999;
-  tree_.q2_ebphi = -999;
-  tree_.q2_ebdelay = -999;
-  tree_.q3_ebeta = -999;
-  tree_.q3_ebphi = -999;
-  tree_.q3_ebdelay = -999;
-  tree_.q4_ebeta = -999;
-  tree_.q4_ebphi = -999;
-  tree_.q4_ebdelay = -999;
-  tree_.q1_hgeta = -999;
-  tree_.q1_hgphi = -999;
-  tree_.q1_hgdelay = -999;
-  tree_.q2_hgeta = -999;
-  tree_.q2_hgphi = -999;
-  tree_.q2_hgdelay = -999;
-  tree_.q3_hgeta = -999;
-  tree_.q3_hgphi = -999;
-  tree_.q3_hgdelay = -999;
-  tree_.q4_hgeta = -999;
-  tree_.q4_hgphi = -999;
-  tree_.q4_hgdelay = -999;
-
 }
 
 
@@ -625,6 +500,24 @@ void Phase2TimingAnalyzer::clearVectors()
   tree_.recojet_HGCALtime.clear();
   tree_.recojet_HGCALenergy.clear();
   tree_.recojet_HGCALnTracksters.clear();
+  tree_.recojet_closestGenIndex.clear();
+  tree_.recojet_closestGenR.clear();
+  tree_.recojet_closestEbGenIndex.clear();
+  tree_.recojet_closestEbGenR.clear();
+  tree_.recojet_closestHgGenIndex.clear();
+  tree_.recojet_closestHgGenR.clear();
+  tree_.q_pt.clear();
+  tree_.q_eta.clear();
+  tree_.q_phi.clear();
+  tree_.q_vx.clear();
+  tree_.q_vy.clear();
+  tree_.q_vz.clear();
+  tree_.q_ebphi.clear();
+  tree_.q_ebeta.clear();
+  tree_.q_ebdelay.clear();
+  tree_.q_hgphi.clear();
+  tree_.q_hgeta.clear();
+  tree_.q_hgdelay.clear();
 }
 
 // ------------ method called once each job just after ending the event loop  ------------
